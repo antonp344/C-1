934e028e83ed6dca19b2e09e836d4b2725d51b19
AntonP <antonp344@gmail.com>
2023-11-17 18:32:22 +0300

    решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -15,17 +15,26 @@
 
 // Задача 2 принимает на вход три числа и выдаёт максимальное из этих чисел.
 
+//Console.Clear();
+//Console.Write("Введите 1-ое число: ");
+//int a = int.Parse(Console.ReadLine()!);
+//Console.Write("Введите 2-ое число: ");
+//int b = int.Parse(Console.ReadLine()!);
+//Console.Write("Введите 3-ое число: ");
+//int c = int.Parse(Console.ReadLine()!);
+//int max = a;
+//if (a > max ) max = a;
+//if (b > max ) max = b;
+//if (c > max ) max = c;
+//Console.WriteLine(max);
+
+
+//Задача 3 на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+
 Console.Clear();
-Console.Write("Введите 1-ое число: ");
-int a = int.Parse(Console.ReadLine()!);
-Console.Write("Введите 2-ое число: ");
-int b = int.Parse(Console.ReadLine()!);
-Console.Write("Введите 3-ое число: ");
-int c = int.Parse(Console.ReadLine()!);
-int max = a;
-if (a > max ) max = a;
-if (b > max ) max = b;
-if (c > max ) max = c;
-Console.WriteLine(max);
-
-
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number % 2==1)
+Console.WriteLine($"Число {number} нечетное");  
+else
+Console.WriteLine($"Число {number} четное");
