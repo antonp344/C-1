30103cc90b607fe3c4c149d0b2944fc704e71132
AntonP <antonp344@gmail.com>
2023-11-17 21:46:27 +0300

    все задачи решены

--- Program.cs
+++ Program.cs
@@ -1,32 +1,32 @@
 ﻿// Задача 1 на вход принимает два числа и выводит, какое число большее, а какое меньшее.
-//Console.Clear();
-//Console.Write("Введите 1-ое число: ");
-//int firstNumber = int.Parse(Console.ReadLine()!);
-//Console.Write("Введите 2-ое число: ");
-//int secondNumber = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
 
-//if (firstNumber > secondNumber)
-//    Console.WriteLine($"Число {firstNumber} больше числа {secondNumber}");
-//    else if (firstNumber < secondNumber)
-//        Console.WriteLine($"Число {firstNumber} меньше числа {secondNumber}");
-//else
-//    Console.WriteLine($"Число {firstNumber} равно числу {secondNumber}");
+if (firstNumber > secondNumber)
+    Console.WriteLine($"Число {firstNumber} больше числа {secondNumber}");
+    else if (firstNumber < secondNumber)
+        Console.WriteLine($"Число {firstNumber} меньше числа {secondNumber}");
+else
+    Console.WriteLine($"Число {firstNumber} равно числу {secondNumber}");
 
 
 // Задача 2 принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-//Console.Clear();
-//Console.Write("Введите 1-ое число: ");
-//int a = int.Parse(Console.ReadLine()!);
-//Console.Write("Введите 2-ое число: ");
-//int b = int.Parse(Console.ReadLine()!);
-//Console.Write("Введите 3-ое число: ");
-//int c = int.Parse(Console.ReadLine()!);
-//int max = a;
-//if (a > max ) max = a;
-//if (b > max ) max = b;
-//if (c > max ) max = c;
-//Console.WriteLine(max);
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2-ое число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3-ое число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+Console.WriteLine(max);
 
 
 //Задача 3 на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
@@ -38,3 +38,13 @@ if (number % 2==1)
 Console.WriteLine($"Число {number} нечетное");  
 else
 Console.WriteLine($"Число {number} четное");
+
+
+//Задача 4 на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), разделеные знаком табуляции.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+for(int i=1; i<=number; i++)
+if (i % 2 ==0)
+Console.Write($"\t {i}");
